84b811739395a49ef9c6fa3cbab3313f4e366017
Unknown <cj.casimiro165@gmail.com>
2017-04-18 17:55:33 -0400

    modification table

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -98,11 +98,13 @@ class UsersController extends AppController
     {
         $this->request->allowMethod(['post', 'delete']);
         $user = $this->Users->get($id);
-        if ($this->Users->delete($user)) {
+      //  $this->Users->Delete($id)=1;
+      /**  if ($this->Users->delete($user)) {
             $this->Flash->success(__('The user has been deleted.'));
         } else {
             $this->Flash->error(__('The user could not be deleted. Please, try again.'));
-        }
+        }*/
+
 
         return $this->redirect(['action' => 'index']);
     }
